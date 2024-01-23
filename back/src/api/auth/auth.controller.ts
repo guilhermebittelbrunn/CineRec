a2907c991fb2558e6d331060ad070909ed24d458
Guilherme Bittelbrunn <guilherme.bitte@unifebe.edu.br>
2024-01-23 11:55:16 -0300

    add 23/01
    create custom decorator - get user

--- back/src/api/auth/auth.controller.ts
+++ back/src/api/auth/auth.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { IToken } from './interfaces/token.interface';
 import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from 'src/common/decoratos/get-user.decorator';
+import { User } from '../users/entities/user.entity';
 
 @Controller('auth')
 @UseGuards(AuthGuard('local'))
@@ -9,7 +11,7 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post()
-  login(@Req() req: any): Promise<IToken> {
-    return this.authService.login(req.user);
+  login(@GetUser() user: User): Promise<IToken> {
+    return this.authService.login(user);
   }
 }
